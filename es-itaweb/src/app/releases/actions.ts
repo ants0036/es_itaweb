d5cbf296ff9b2f40eef1e5d370ee6c5b36eab067
Anthony Su <anthony.s0036@gmail.com>
2024-09-23 10:17:29 -0400

    null checks

--- es-itaweb/src/app/releases/actions.ts
+++ es-itaweb/src/app/releases/actions.ts
@@ -7,14 +7,14 @@ export async function update (formData: FormData) {
     const supabase = createClient();
     const {data: { user }} = await supabase.auth.getUser();
 
+    if (user == null) {
+        return;
+    }
+
     const count = formData.get('count');
     const i_id = formData.get('i_id');
     const r_id = formData.get('r_id');
     const variant = formData.get('variant');
-    /*console.log(count)
-    console.log(user.id)
-    console.log(i_id)
-    console.log(r_id)*/
 
     const { data, error } = await supabase.from('user_data').upsert({qty: count, user_id: user.id, r_id: r_id, i_id: i_id, variant:variant}, {onConflict: 'r_id, i_id, variant, user_id'})
     
